f06f42ec86f9d149983eb3f678a08c37c855a883
fytrw <you@example.com>
2023-06-06 17:37:35 +0200

    app.jsx custom hook and fetch change

--- src/App.jsx
+++ src/App.jsx
@@ -5,79 +5,86 @@ import Error from './components/Error'
 import './style.css'
 import { useState, useEffect } from 'react'
 
+// Custom hook for setting CSS variables
+const useUpdateCSSVariables = (isDarkMode, fontStyle) => {
+  useEffect(() => {
+    const styles = isDarkMode ? {
+      '--color-text': '#FFFFFF',
+      '--bg-color': '#050505',
+      '--hint-text-color': '#A445ED',
+      '--color-second-text': '#757575',
+      '--error-text-color': '#FF5252',
+      '--search-bar-color': '#1F1F1F',
+      '--box-shadow-color': 'rgba(164, 69, 237, 0.6)',
+    } : {
+      '--color-text': '#2D2D2D',
+      '--bg-color': '#FFFFFF',
+      '--hint-text-color': '#A445ED',
+      '--color-second-text': '#757575',
+      '--error-text-color': '#FF5252',
+      '--search-bar-color': '#F4F4F4',
+      '--box-shadow-color': 'rgba(0, 0, 0, 0.2)',
+    };
+
+    const fonts = {
+      'Sans Serif': 'Inter, sans-serif',
+      'Serif': 'Lora, serif',
+      'Mono': 'Inconsolata, monospace',
+    };
+
+    Object.entries(styles).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
+
+    document.documentElement.style.setProperty('--font-family', fonts[fontStyle]);
+  }, [isDarkMode, fontStyle]);
+};
+
+// Async function for fetching search results
+const fetchSearchResults = async (searchTerm, setSearchResult, setError) => {
+  if (!searchTerm) return;
+
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    setSearchResult(data);
+    setError(false);
+  } catch (error) {
+    setError(true);
+  }
+};
+
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [fontStyle, setFontStyle] = useState('Sans Serif');
-  //searching
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResult, setSearchResult] = useState(null);
   const [inputValue, setInputValue] = useState('');
   const [error, setError] = useState(false);
-  // here we set the CSS variables
+
+  // Use custom hook for setting CSS variables
+  useUpdateCSSVariables(isDarkMode, fontStyle);
+
+  // Fetch search results when searchTerm changes
   useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.style.setProperty('--color-text', '#FFFFFF');
-      document.documentElement.style.setProperty('--bg-color', '#050505');
-      document.documentElement.style.setProperty('--hint-text-color', '#A445ED');
-      document.documentElement.style.setProperty('--color-second-text', '#757575');
-      document.documentElement.style.setProperty('--error-text-color', '#FF5252');
-      document.documentElement.style.setProperty('--search-bar-color', '#1F1F1F');
-      document.documentElement.style.setProperty('--box-shadow-color', 'rgba(164, 69, 237, 0.6)');
-    } else {
-      document.documentElement.style.setProperty('--color-text', '#2D2D2D');
-      document.documentElement.style.setProperty('--bg-color', '#FFFFFF');
-      document.documentElement.style.setProperty('--hint-text-color', '#A445ED');
-      document.documentElement.style.setProperty('--color-second-text', '#757575');
-      document.documentElement.style.setProperty('--error-text-color', '#FF5252');
-      document.documentElement.style.setProperty('--search-bar-color', '#F4F4F4');
-      document.documentElement.style.setProperty('--box-shadow-color', 'rgba(0, 0, 0, 0.2)');
-    }
-  }, [isDarkMode]); // this effect will run when isDarkMode changes
+    fetchSearchResults(searchTerm, setSearchResult, setError);
+  }, [searchTerm]);
 
-    // here we set the font style
-    useEffect(() => {
-      if (fontStyle === 'Sans Serif') {
-        document.documentElement.style.setProperty('--font-family', 'Inter, sans-serif');
-      } else if (fontStyle === 'Serif') {
-        document.documentElement.style.setProperty('--font-family', 'Lora, serif');
-      } else if (fontStyle === 'Mono') {
-        document.documentElement.style.setProperty('--font-family', 'Inconsolata, monospace');
-      }
-    }, [fontStyle]); // this effect will run when fontStyle changes
+  let displayedResult = error ? <Error /> : (searchResult && <Main searchResult={searchResult} setSearchTerm={setSearchTerm} setInputValue={setInputValue}/>);
 
-    useEffect(() => {
-      if (searchTerm !== '') {
-        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(data => {
-            setSearchResult(data);
-            console.log(data);
-            setError(false);
-          })
-          .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-            setError(true);
-          });
-      }
-    }, [searchTerm]);
-    let displayedResult = null;
-  if(error) {
-    displayedResult = <Error />
-  } else if (searchResult) {
-    displayedResult = <Main searchResult={searchResult} setSearchTerm={setSearchTerm} setInputValue={setInputValue}/>
-  }
   return (
-      <div className="wrapper">
-        <Header darkMode={isDarkMode} setDarkMode={setIsDarkMode} fontStyle={fontStyle} setFontStyle={setFontStyle}/>
-        <SearchBar inputValue={inputValue} setInputValue={setInputValue} setSearchTerm={setSearchTerm}/>
-        {displayedResult}
-      </div>
+    <div className="wrapper">
+      <Header darkMode={isDarkMode} setDarkMode={setIsDarkMode} fontStyle={fontStyle} setFontStyle={setFontStyle}/>
+      <SearchBar inputValue={inputValue} setInputValue={setInputValue} setSearchTerm={setSearchTerm}/>
+      {displayedResult}
+    </div>
   )
 }
 
 export default App
+
